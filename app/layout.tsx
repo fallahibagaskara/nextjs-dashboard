a85edea3e3b475528fdc8d34a26634cc3e26d2c3
Fallahi Bagaskara <fallahibagaskara@gmail.com>
2024-08-29 18:06:57 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+ 
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="description" content="A brief description of the page content." />
+      <meta name="keywords" content="keyword1, keyword2, keyword3" />
+      <meta property="og:title" content="Title Here" />
+      <meta property="og:description" content="Description Here" />
+      <meta property="og:image" content="image_url_here" />
+      <link rel="icon" href="path/to/favicon.ico" />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
